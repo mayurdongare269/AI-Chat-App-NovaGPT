3a5b63083e8c63493a2e86cff83e8e83cec4ebc5
Mayur Dongare <mayurdongare269@gmail.com>
2026-04-13 16:29:38 +0530

    database(mongo) connection done

--- Backend/server.js
+++ Backend/server.js
@@ -19,11 +19,14 @@
 
 // %##################################
 
+
+
 // method 2
 import express from "express";
 import 'dotenv/config';
 import cors from "cors";
 import { GoogleGenAI } from "@google/genai";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 8080;
@@ -31,34 +34,51 @@ const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 
-const ai = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY,
-});
-
-app.post("/chat", async(req, res) => {
-    try {
-        const {message} = req.body;
-
-        const response = await ai.models.generateContent({
-            model: "gemini-3-flash-preview",
-            contents: message,
-        })
-
-        console.log(response); // debug
-        // const reply = response.response.text();
-        // const reply = response?.response?.text() || "No response from AI";
-
-        const reply = response?.candidates?.[0]?.content?.parts?.[0]?.text || "No response from AI";
-        
-
-        res.json({reply});
-
-    }catch(err) {
-        console.error(err);
-        res.status(500).json({error: "something went wrong"});
-    }
-});
-
 app.listen(PORT, () => {
     console.log(`server is running  on ${PORT}`);
+    connectDB(); // first check  is connection established or not with db...
 })
+
+//mongo connection
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
+    }catch(err) {
+        console.log("failed to connect", err);
+    }
+}
+
+
+
+
+
+//below code logic in in utils...
+// const ai = new GoogleGenAI({
+//     apiKey: process.env.GEMINI_API_KEY,
+// });
+
+// app.post("/chat", async(req, res) => {
+//     try {
+//         const {message} = req.body;
+
+//         const response = await ai.models.generateContent({
+//             model: "gemini-3-flash-preview",
+//             contents: message,
+//         })
+
+//         console.log(response); // debug
+//         // const reply = response.response.text();
+//         // const reply = response?.response?.text() || "No response from AI";
+
+//         const reply = response?.candidates?.[0]?.content?.parts?.[0]?.text || "No response from AI";
+        
+
+//         res.json({reply});
+
+//     }catch(err) {
+//         console.error(err);
+//         res.status(500).json({error: "something went wrong"});
+//     }
+// });
+
