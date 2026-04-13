0f9cecbc76310d5d1b20bdcc2900bdd56033ea26
Mayur Dongare <mayurdongare269@gmail.com>
2026-04-14 00:19:47 +0530

    sidebar but errors

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,19 @@
 
 import './App.css'
+import Sidebar from './Sidebar.jsx';
+import ChatWindow from './ChatWindow.jsx';
+import {MyContext} from './MyContext.jsx';
 
 function App() {
+  const providerValues = {}; // passing values
 
   return (
-    <>
-      
-    </>
+    <div className='main'>
+      <MyContext.Provider values={providerValues}>
+        <Sidebar></Sidebar>
+        <ChatWindow></ChatWindow>
+      </MyContext.Provider>
+    </div>
   )
 }
 
--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -2,7 +2,9 @@ import "./ChatWindow.css";
 
 function ChatWindow() {
     return (
-        <></>
+        <div>
+            ChatWindow
+        </div>
     )
 }
 
